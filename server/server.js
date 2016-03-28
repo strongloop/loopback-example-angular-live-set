d00361a9308f34e3a75d027bfc3ea4d85ab50d9c
Candy <ngcandy@ca.ibm.com>
2016-03-28 15:57:43 -0400

    Add missing baseUrl variable

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
+    var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', app.get('url'));
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
